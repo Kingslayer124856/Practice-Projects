d7dfee3e6d0886eb2212239da45b456eb746d185
Kingslayer124856 <cassandra.king4@my.jcu.edu.au>
2024-11-17 14:22:09 +1000

    Adding the multiply and divide functions

--- Test Programs/Calculator.py
+++ Test Programs/Calculator.py
@@ -1,7 +1,7 @@
 """
 Project Calculator
 Created on: 1/10/2023
-Edited 1:
+Edited 1: 17/11/2024
 Made by: Cassandra King 
 """
 # add other options besides addition like the basic 4: 
@@ -9,14 +9,26 @@ Made by: Cassandra King
 def main():
     """Menu of calculation options """
 def addition():
+    """Calculating the addition of 2 numbers"""
     num1 = float(input("First Number: "))
     num2 = float(input("Second Number: "))
     sum = num1 + num2
     print("Sum: ", sum)
 def subtraction():
+    """Calculating the subtraction of 2 numbers"""
     num1 = float(input("First Number: "))
     num2 = float(input("Second Number: "))
     sum = num1 - num2
     print("Sum: ", sum)
-
-subtraction()
+def multipy():
+    """Calculating the multiplication of 2 numbers"""
+    num1 = float(input("First Number: "))
+    num2 = float(input("Second Number: "))
+    sum = num1 * num2
+    print("Sum: ", sum)
+def divide():
+    """Calculating the division of 2 numbers"""
+    num1 = float(input("First Number: "))
+    num2 = float(input("Second Number: "))
+    sum = num1 / num2
+    print("Sum: ", sum)

2668668790dff6dac49205bc24b8f904d6a6efd8
Kingslayer124856 <cassandra.king4@my.jcu.edu.au>
2024-11-17 14:17:07 +1000

    Changing addition into a function and adding a subtraction function.Also, the start of the main.

--- Test Programs/Calculator.py
+++ Test Programs/Calculator.py
@@ -4,9 +4,19 @@ Created on: 1/10/2023
 Edited 1:
 Made by: Cassandra King 
 """
-num1 = float(input("First Number: "))
-num2 = float(input("Second Number: "))
-sum = num1 + num2
-print("Sum: ", sum)
 # add other options besides addition like the basic 4: 
 # addition, subtration, multiplication and division
+def main():
+    """Menu of calculation options """
+def addition():
+    num1 = float(input("First Number: "))
+    num2 = float(input("Second Number: "))
+    sum = num1 + num2
+    print("Sum: ", sum)
+def subtraction():
+    num1 = float(input("First Number: "))
+    num2 = float(input("Second Number: "))
+    sum = num1 - num2
+    print("Sum: ", sum)
+
+subtraction()

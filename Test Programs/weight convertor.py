26ae4ba9785ed745ef9f2904cbd9ebfebb7ba347
Kingslayer124856 <cassandra.king4@my.jcu.edu.au>
2025-05-29 22:52:38 +1000

    possible future changes in comment

--- Test Programs/weight convertor.py
+++ Test Programs/weight convertor.py
@@ -1,3 +1,11 @@
+"""
+Project weight converter
+Made by: Cassandra King
+Edit date:
+"""
+#could add more weight options and conversions
+# selection of type of weight into another selection, will need formulas for each weight type
+
 weight = float(input("Weight: "))
 type = input("(K)g or (L)bs: ").lower()
 if type == "k":
